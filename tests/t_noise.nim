f98c5ce5e1350a0c62e8c29ea539cf87adac1c98
Nycto <James@RoundEights.com>
2019-03-27 08:26:25 -0700

    Add test for build time noise generation

--- tests/t_noise.nim
+++ tests/t_noise.nim
@@ -14,3 +14,11 @@ suite "General Noise should":
         check( noise.get(NoiseType.simplex, 1, 2, 3) >= 0 )
         check( noise.get(NoiseType.simplex, 1, 2, 3) < 1 )
 
+    test "Noise should be assignable at build time":
+        const noise = newNoise(12345)
+        const simplexVal = noise.get(NoiseType.simplex, 1, 2, 3)
+        const noiseVal = noise.get(NoiseType.perlin, 1, 2, 3)
+
+        check(simplexVal == 0.3310572195555556)
+        check(noiseVal == 0.492089610128111)
+
